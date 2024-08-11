30ac5d0c3e440471abfded694feaf009a712e137
bahramteymoori1994 <70304284+bahramteymoori1994@users.noreply.github.com>
2024-08-11 20:52:44 +0330

    Manager is done

--- src/storeshop/Main.java
+++ src/storeshop/Main.java
@@ -1,28 +1,20 @@
 package storeshop;
 
 import storeshop.controllers.CustomerController;
+import storeshop.controllers.ManagerController;
+import storeshop.model.entities.Manager;
 
 public class Main {
     public static void main(String[] args) throws Exception {
 
-        //        Customer customer = new Customer();
-//        customer
-//                .setCustomerName("jaber")
-//                .setCustomerFamily(null)
-//                .setCustomerNationalCode("9999333210")
-//                .setCustomerCellPhone("09123330011")
-//                .setCustomerEmail("jaber.jabari@gmail.com");
+        Manager manager = new Manager();
+        manager
+                .setManagerName("farzad")
+                .setManagerFamily("farzadi")
+                .setManagerNationalCode("7775300000")
+                .setManagerCellPhone("09127775522");
 
-        CustomerController customerController = new CustomerController();
-        System.out.println(customerController.findById(8));
-
-        //        if( customerController.add(customer) == null )
-//        {
-//            System.out.println("Data is not added");
-//        }
-//        else
-//        {
-//            System.out.println(customerController.add(customer));
-//        }
+        ManagerController managerController = new ManagerController();
+        System.out.println(managerController.findAll());
     }
 }
--- src/storeshop/controllers/ManagerController.java
+++ src/storeshop/controllers/ManagerController.java
@@ -1,4 +1,73 @@
 package storeshop.controllers;
 
-public class ManagerController {
+import storeshop.exception.ExceptionWrapper;
+import storeshop.model.entities.Customer;
+import storeshop.model.entities.Manager;
+import storeshop.services.ManagerService;
+
+import java.sql.SQLException;
+import java.sql.SQLIntegrityConstraintViolationException;
+import java.util.List;
+
+public class ManagerController implements AbstractBaseController<Manager>
+{
+    ManagerService managerService = new ManagerService();
+
+    @Override
+    public Manager add(Manager manager) throws Exception {
+        try
+        {
+            return managerService.add(manager);
+        }
+        catch( SQLIntegrityConstraintViolationException e )
+        {
+            System.out.println(ExceptionWrapper.getMessage(manager, e));
+        }
+        return null;
+    }
+
+    @Override
+    public Manager update(Manager manager) throws Exception {
+        return managerService.update(manager);
+    }
+
+    @Override
+    public void remove(long id) throws Exception {
+
+        Manager manager = new Manager();
+
+        try
+        {
+            if( id == managerService.findById(id).getManagerId() )
+            {
+                managerService.remove(id);
+            }
+        }
+        catch(SQLException e)
+        {
+            System.out.println(ExceptionWrapper.getMessage(manager, e));
+        }
+    }
+
+    @Override
+    public Manager findById(long id) throws Exception {
+
+        Customer customer = new Customer();
+
+        try
+        {
+            return managerService.findById(id);
+        }
+        catch(Exception e)
+        {
+            System.out.println(ExceptionWrapper.getMessage(customer, e));
+        }
+
+        return null;
+    }
+
+    @Override
+    public List<Manager> findAll() throws Exception {
+        return managerService.findAll();
+    }
 }
